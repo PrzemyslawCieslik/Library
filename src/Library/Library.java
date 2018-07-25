db3286230858ad9043e4526e4c7d51eceb31872c
Przemyslaw <cprzemek91@hotmail.com>
2018-07-25 19:31:51 +0100

    Library v0.1

---
+++ src/Library/Library.java
@@ -0,0 +1,22 @@
+package Library;
+
+public class Library {
+    public static void main (String[] args){
+        final String appName = "Library v0.1";
+
+        String title = "Thinking in Java";
+        String author = "Bruce Eckel";
+        int releaseDate = 2006;
+        int pages = 1248;
+        String publisher = "Helion";
+        String isbn =  "9788328334427";
+
+        System.out.println(appName);
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releaseDate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+    }
+}
