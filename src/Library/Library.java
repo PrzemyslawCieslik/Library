67e32ede6c667710deb3b425c9a146bca678db6c
Przemyslaw <cprzemek91@hotmail.com>
2018-07-26 17:48:04 +0100

    constructor and method

--- src/Library/Library.java
+++ src/Library/Library.java
@@ -2,21 +2,18 @@ package Library;
 
 public class Library {
     public static void main (String[] args){
-        final String appName = "Library v0.1";
+        final String appName = "Library v0.2";
 
-        String title = "Thinking in Java";
-        String author = "Bruce Eckel";
-        int releaseDate = 2006;
-        int pages = 1248;
-        String publisher = "Helion";
-        String isbn =  "9788328334427";
+        Book book1 = new Book("Thinking in Java", "Bruce Eckel", 2006, 1248,
+        "Helion","9788328334427");
+        Book book2 = new Book("Clean Code: A Handbook of Agile Software Craftsmanship","Robert C. Martin",
+                2008, 464, "Prentice Hall","0132350882");
+
 
         System.out.println(appName);
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(releaseDate);
-        System.out.println(pages);
-        System.out.println(publisher);
-        System.out.println(isbn);
+        book1.printInfo();
+        book2.printInfo();
+
+
     }
 }
