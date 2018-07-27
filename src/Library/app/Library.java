5fc6346a5d6ef8b8b32e238405686a6d9f14e187
Przemyslaw <cprzemek91@hotmail.com>
2018-07-27 19:36:54 +0100

    add DataReader and change code in app/Library

--- src/Library/app/Library.java
+++ src/Library/app/Library.java
@@ -1,19 +1,18 @@
 package Library.app;
 import Library.data.Book;
+import Library.utils.dataReader;
 
 public class Library {
     public static void main (String[] args){
         final String appName = "Library v0.3";
         Book[] books = new Book[1000];
-        books[0] = new Book("Thinking in Java", "Bruce Eckel", 2006, 1248,
-        "Helion","9788328334427");
-        books[1] = new Book("Clean Code: A Handbook of Agile Software Craftsmanship","Robert C. Martin",
-                2008, 464, "Prentice Hall","0132350882");
+        dataReader dr = new dataReader();
 
 
         System.out.println(appName);
-        books[0].printInfo();
-        books[1].printInfo();
+        books[0] = dr.readAndPrint();
+        books[1] = dr.readAndPrint();
+        dr.close();
         System.out.println("In our library we can handle "+books.length+" books");
 
 
